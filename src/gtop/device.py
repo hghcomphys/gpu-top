8adbc3d1226165514d9c11df8acfa388f737d8a0
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2026-04-23 23:21:26 +0200

    fix issue for no GPU machine

--- src/gtop/device.py
+++ src/gtop/device.py
@@ -4,9 +4,11 @@ from typing import Tuple
 import pynvml
 
 DeviceHandle = pynvml.struct_c_nvmlDevice_t
+pynvml_available = True
 
 
 def get_devices() -> Tuple[DeviceHandle, ...]:
+    global pynvml_available
     try:
         pynvml.nvmlInit()
         device_count = pynvml.nvmlDeviceGetCount()
@@ -14,8 +16,11 @@ def get_devices() -> Tuple[DeviceHandle, ...]:
         return handles
     except pynvml.NVMLError as error:
         print(f"GPU Not Detected! (Error: {error})")
+        pynvml_available = False
         sys.exit(1)
 
 
 def free_devices() -> None:
-    pynvml.nvmlShutdown()
+    global pynvml_available
+    if pynvml_available:
+        pynvml.nvmlShutdown()
